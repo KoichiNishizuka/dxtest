76ca67598aa31ccccc4875ab58550a785db01dcf
Taku75 <ta9mizoids@gmail.com>
2018-12-27 03:29:14 +0900

    add player model

--- dxtest/dxtest.cpp
+++ dxtest/dxtest.cpp
@@ -67,8 +67,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	int addi=0;
 		 
-	int  remain=6;
-
+	int  remain=5;
+	int g = 0;
 	int sleep = 0;//ちょっとだけ待とうか
 	float angleX = 0.f, angleY = 0.f;
 
@@ -86,7 +86,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	int plane = MV1LoadModel("C:\\Users\\Angelic Angel\\source\\repos\\dxtest\\plane\\Plane.x");
 	int Soccerfield = MV1LoadModel("C:\\Users\\Angelic Angel\\source\\repos\\dxtest\\soccer field\\Soccer Grount Base high optimised.blend.x");
 	int Soccergoal = MV1LoadModel("C:\\Users\\Angelic Angel\\source\\repos\\dxtest\\Soccer goal\\goal low optimised.blend.x");
-	int Soccerballx = MV1LoadModel("C:\\Users\\Angelic Angel\\source\\repos\\dxtest\\Soccer ball\\Soccer Ball Low.blend.x");
+	
+	//サッカーボールの画像の読み込み
+	int soccerHAndle = LoadGraph("C:\\Users\\Angelic Angel\\source\\repos\\dxtest\\soccerball.jpg");
 	/*// モデルに含まれるテクスチャの総数を取得する
 	Grassllllow = MV1GetTextureNum(Soccerfield);
 
@@ -131,7 +133,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	//SetCameraPositionAndTarget_UpVecY(VGet(30, 20, 0), VGet(10.0f, 10.0f, 0.0f));
 	
 	//この点でbox=S;
-	DrawFormatString(240, 240, GetColor(255, 255, 255), " Xキーを押してください");
+	DrawFormatString(240, 300, GetColor(255, 255, 255), " Xキーを押してください");
 	ScreenFlip();
 	while (CheckHitKey(KEY_INPUT_X) != 1)
 	{
@@ -154,7 +156,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 			addi = 0; sleep = 0; x = 0; y = 1; z = 0;
 			angleX = 0.f; angleY = 0.f;
 			t = 0;
-			
+
 			vx = -5.0f; vy = 10.0f; vz = 5.0;//のちのち相手からもらう部分のループに入れる必要あり。
 
 		}
@@ -164,137 +166,137 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		//MV1SetPosition(plane, VGet(0, 10, 0));
 		MV1SetPosition(Soccerfield, VGet(1, 10.1, 1));
 		MV1SetPosition(Soccergoal, VGet(-30, 0, 0));
-		MV1SetPosition(Soccerballx, VGet(290, 290, 0));
+		
 
 		// ３Ｄモデルの描画
 		MV1DrawModel(Soccerball);
 		//MV1DrawModel(plane);
 		MV1DrawModel(Soccerfield);
 		MV1DrawModel(Soccergoal);
-		MV1DrawModel(Soccerballx);
-
+		
 		MV1SetRotationXYZ(Soccerball, VGet(angleX, angleY, 0.0f));
 
 		if (!ini) {
 
 		}
-
-		while (!ini)
+		for (int g = 0; g < remain; g++)
 		{
-
-			ClearDrawScreen();
-			if (CheckHitKey(KEY_INPUT_ESCAPE)) {
-				ini = true;
-				box = 'I';													//バイト型で送るもの
-				buffer[0] = box;
-			}
-
-
-			if (CheckHitKey(KEY_INPUT_RIGHT)) {
-				ClearDrawScreen();
-				
-				thy += 15;
-				if (thy <= 180) {
-					cz = r * (cos((float)thz*(PI / 180.0f))*sin((float)thy*(PI / 180.0f)));
-					cx = r * (cos((float)thz*(PI / 180.0f))*cos((float)thy*(PI / 180.0f))) + tx;
-				}
-				else {
-					thy = 180;
-				}
-			}
-			else if (CheckHitKey(KEY_INPUT_LEFT)) {
-				ClearDrawScreen();
-				thy -= 15;
-				if (thy >= -180) {
-					cz = r * (cos((float)thz*(PI / 180.0f))*sin((float)thy*(PI / 180.0f)));
-					cx = r * (cos((float)thz*(PI / 180.0f))*cos((float)thy*(PI / 180.0f))) + tx;
-				}
-				else {
-					thy = -180;
-				}
-
-			}
-
-			if (CheckHitKey(KEY_INPUT_UP)) {
-				ClearDrawScreen();
-				thz += 15;
-
-				if (thz <= 90) {
-
-
-					cy = r * (sin((float)thz*(PI / 180.0f)));
-					cz = r * (cos((float)thz*(PI / 180.0f))*sin((float)thy*(PI / 180.0f)));
-					cx = r * (cos((float)thz*(PI / 180.0f))*cos((float)thy*(PI / 180.0f))) + tx;
-					//cx = 30 * (cos((float)thz*(PI / 180.0f))) - 21.0f;
-				}
-				else {
-					thz = 90;
-				}
-			}
-			else if (CheckHitKey(KEY_INPUT_DOWN)) {
-				ClearDrawScreen();
-				thz -= 15;
-
-				if (thz >= 0) {
-
-					cy = r * (sin((float)thz*(PI / 180.0f)));
-					cz = r * (cos((float)thz*(PI / 180.0f))*sin((float)thy*(PI / 180.0f)));
-					cx = r * (cos((float)thz*(PI / 180.0f))*cos((float)thy*(PI / 180.0f))) + tx;
-					//cx = 30 * (cos((float)thz*(PI / 180.0f))) - 21.0f;
-				}
-				else {
-					thz = 0;
-				}
-			}
-
-			for (int g = 0; g < remain; g++)
+			while (!ini)
 			{
+
+				ClearDrawScreen();
+				x = 0;
+				y = 1;
+				z = 0; angleX = 0.f; angleY = 0.f;
+				t = 0;
+				vx = -5.0f; vy = 10.0f; vz = 5.0;
+
+				if (CheckHitKey(KEY_INPUT_ESCAPE)) {
+					ini = true;
+					box = 'I';													//バイト型で送るもの
+					buffer[0] = box;
+				}
+
+
+				if (CheckHitKey(KEY_INPUT_RIGHT)) {
+					ClearDrawScreen();
+
+					thy += 15;
+					if (thy <= 180) {
+						cz = r * (cos((float)thz*(PI / 180.0f))*sin((float)thy*(PI / 180.0f)));
+						cx = r * (cos((float)thz*(PI / 180.0f))*cos((float)thy*(PI / 180.0f))) + tx;
+					}
+					else {
+						thy = 180;
+					}
+				}
+				else if (CheckHitKey(KEY_INPUT_LEFT)) {
+					ClearDrawScreen();
+					thy -= 15;
+					if (thy >= -180) {
+						cz = r * (cos((float)thz*(PI / 180.0f))*sin((float)thy*(PI / 180.0f)));
+						cx = r * (cos((float)thz*(PI / 180.0f))*cos((float)thy*(PI / 180.0f))) + tx;
+					}
+					else {
+						thy = -180;
+					}
+
+				}
+
+				if (CheckHitKey(KEY_INPUT_UP)) {
+					ClearDrawScreen();
+					thz += 15;
+
+					if (thz <= 90) {
+
+
+						cy = r * (sin((float)thz*(PI / 180.0f)));
+						cz = r * (cos((float)thz*(PI / 180.0f))*sin((float)thy*(PI / 180.0f)));
+						cx = r * (cos((float)thz*(PI / 180.0f))*cos((float)thy*(PI / 180.0f))) + tx;
+						//cx = 30 * (cos((float)thz*(PI / 180.0f))) - 21.0f;
+					}
+					else {
+						thz = 90;
+					}
+				}
+				else if (CheckHitKey(KEY_INPUT_DOWN)) {
+					ClearDrawScreen();
+					thz -= 15;
+
+					if (thz >= 0) {
+
+						cy = r * (sin((float)thz*(PI / 180.0f)));
+						cz = r * (cos((float)thz*(PI / 180.0f))*sin((float)thy*(PI / 180.0f)));
+						cx = r * (cos((float)thz*(PI / 180.0f))*cos((float)thy*(PI / 180.0f))) + tx;
+						//cx = 30 * (cos((float)thz*(PI / 180.0f))) - 21.0f;
+					}
+					else {
+						thz = 0;
+					}
+				}
+
+
 				switch (g)
 				{
 				case 0:
-					
-					DrawFormatString(600,20, GetColor(255, 255, 255), "×5");
-					ScreenFlip();
+
+					DrawFormatString(600, 20, GetColor(0, 0, 255), "×5");
+					//ScreenFlip();
 					break;
 
 
 				case 1:
-					
-					DrawFormatString(600, 20, GetColor(255, 255, 255), "×4");
-					ScreenFlip();
+
+					DrawFormatString(600, 20, GetColor(0, 0, 255), "×4");
+					//ScreenFlip();
 					break;
 
 				case 2:
-					
-					DrawFormatString(600,20, GetColor(255, 255, 255), "×3");
-					ScreenFlip();
+
+					DrawFormatString(600, 20, GetColor(0, 0, 255), "×3");
+					//ScreenFlip();
 					break;
 
 
 				case 3:
-		
-					DrawFormatString(600, 20, GetColor(255, 255, 255), "×2");
-					ScreenFlip();
+
+					DrawFormatString(600, 20, GetColor(0, 0, 255), "×2");
+					//ScreenFlip();
 					break;
 
 
 				case 4:
-					
-					DrawFormatString(600, 20, GetColor(255, 255, 255), "×1");
-					ScreenFlip(); 
+
+					DrawFormatString(600, 20, GetColor(0, 0, 255), "×1");
+					//ScreenFlip();
 					break;
 
 
-				case 5:
-					
-					
-					DrawFormatString(600, 20, GetColor(255, 255, 255), "×0");
-					ScreenFlip();
-					break;
+				
 
 				}
-			}
-			ClearDrawScreen();
+				
+				
 
 
 				// 指定位置にモデルを配置
@@ -308,102 +310,135 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 				//MV1DrawModel(plane);
 				MV1DrawModel(Soccerfield);
 				MV1DrawModel(Soccergoal);
-				MV1DrawModel(Soccerballx);
-           
-				DrawFormatString(240, 240, GetColor(255, 255, 255), "Escキーを押してください");
+				
+				DrawFormatString(240, 300, GetColor(255, 255, 255), "Escキーを押してください");
 
 				DrawFormatString(50, 50, GetColor(255, 255, 255), "%3f.4,%3f.4,%3f.4", cx, cy, cz);
 
 				SetCameraPositionAndTarget_UpVecY(VGet(cx, cy, cz), VGet(tx, 0.0f, 0.0f));
-
+				DrawExtendGraph(590, 20, 590 - 50, 20 + 50, soccerHAndle, FALSE);
 				ScreenFlip();
 				Sleep(100);
-			
-		}
 
-		//ここでボールをける前に加速度の初期化
-		
-		
-		
-		if (sleep > 120) {//二秒待ってから→fpsを定義する時間
-			t += 1.0f / fps.mFps;
-			
-			if (x > -28) {	//ゴールについた時の条件
-				x += vx * (1.0f / fps.mFps);
-				angleX -= ROTATE_SPEED;
-				if (CheckHitKey(KEY_INPUT_Z)) 
-				{
-					//if (z > -9 && z < 9)
-					z += vz * (1.0f / fps.mFps);
-					angleY += 0.99f*ROTATE_SPEED;
-				}
-				if (CheckHitKey(KEY_INPUT_C)) 
-				{
-					//if(z>-9&&z<9)
-					z -= vz * (1.0f / fps.mFps);
-					angleY += 0.99f*ROTATE_SPEED;
-				}
 			}
-			if (y >= 1)	//地面よりも高い時の条件
-				
-				y = vy * t - 0.2f*G*t*t + 1.0f;
-			
-			else {
 
-				if (vx <= 0) {
-					vx = 0.998f*vx;
-						
-				}
-			}
-			if (x <= -20.4)
+			//ここでボールをける前に加速度の初期化
+
+			while (ini)
 			{
-				//ゴールポストの判定
-				if (/*x >= -20.4 && y < 11 && z <= 10.4 && z >= 9.6 ||*/ x >= -20.4 &&x<=-20.3&& y < 11/* && z >= -10.4&&z <= -9.6 /*|| x >= -20.4 && y <11 && y>10 && -10.4 <= z <= 10.4*/)
-				{
-					x  -= vx * (1.0f / fps.mFps);
-					DrawFormatString(50, 50, GetColor(255, 255, 255), "reflect");
-				}
+				fps.Update();	//更新
+		   //ScreenFlip();
+				fps.Wait();		//待機
 
-				addi++;
-				if (y <= 10 && z >= -9.6 && 9.6 >= z) {
-					DrawFormatString(50, 50, GetColor(255, 255, 255), "GOAL!!!!!!!!");
-					//con = 'CON';
-					if (addi == 1) {
-						box = 'G';												//バイト型で送るもの
-						buffer[0] = box;
-					}
+				ClearDrawScreen();
+				// 指定位置にモデルを配置
+				MV1SetPosition(Soccerball, VGet(x, y, z));
+				//MV1SetPosition(plane, VGet(0, 10, 0));
+				MV1SetPosition(Soccerfield, VGet(1, 10.1, 1));
+				MV1SetPosition(Soccergoal, VGet(-30, 0, 0));
 
-					if (addi > 300) {
-						ini = false;
-					}
-					
-				}
-
-				else
-				{
-					DrawFormatString(50, 50, GetColor(255, 255, 255), "m9(^Д^)ﾌﾟｷﾞｬｰ");
-					if (addi == 1) {
-						box = 'O';												//バイト型で送るもの
-						buffer[0] = box;
-					}
-
-					if (addi > 300) {
-						ini = false;
-					}
-				}
+				// ３Ｄモデルの描画
+				MV1DrawModel(Soccerball);
+				//MV1DrawModel(plane);
+				MV1DrawModel(Soccerfield);
+				MV1DrawModel(Soccergoal);
 				
 
+				MV1SetRotationXYZ(Soccerball, VGet(angleX, angleY, 0.0f));
 				
+				if (sleep > 120) {//二秒待ってから→fpsを定義する時間
+					t += 1.0f / fps.mFps;
+
+					if (x > -28) {	//ゴールについた時の条件
+						x += vx * (1.0f / fps.mFps);
+						angleX -= ROTATE_SPEED;
+						if (CheckHitKey(KEY_INPUT_Z))
+						{
+							//if (z > -9 && z < 9)
+							z += vz * (1.0f / fps.mFps);
+							angleY += 0.99f*ROTATE_SPEED;
+						}
+						if (CheckHitKey(KEY_INPUT_C))
+						{
+							//if(z>-9&&z<9)
+							z -= vz * (1.0f / fps.mFps);
+							angleY += 0.99f*ROTATE_SPEED;
+						}
+					}
+					if (y >= 1)	//地面よりも高い時の条件
+
+						y = vy * t - 0.2f*G*t*t + 1.0f;
+
+					else {
+
+						if (vx <= 0) {
+							vx = 0.998f*vx;
+
+						}
+					}
+					if (x <= -20.4)
+					{
+						//ゴールポストの判定
+						if (/*x >= -20.4 && y < 11 && z <= 10.4 && z >= 9.6 ||*/ x >= -20.4 &&x <= -20.3&& y < 11/* && z >= -10.4&&z <= -9.6 /*|| x >= -20.4 && y <11 && y>10 && -10.4 <= z <= 10.4*/)
+						{
+							x -= vx * (1.0f / fps.mFps);
+							DrawFormatString(50, 50, GetColor(255, 255, 255), "reflect");
+						}
+
+						addi++;
+						if (y <= 10 && z >= -9.6 && 9.6 >= z) {
+							DrawFormatString(50, 50, GetColor(255, 255, 255), "GOAL!!!!!!!!");
+							//con = 'CON';
+							if (addi == 1) {
+								box = 'G';												//バイト型で送るもの
+								buffer[0] = box;
+							}
+
+							if (addi > 300) {
+								ini = false;
+								addi = 0;
+								sleep = 0;
+							}
+
+						}
+
+						else
+						{
+							DrawFormatString(50, 50, GetColor(255, 255, 255), "m9(^Д^)ﾌﾟｷﾞｬｰ");
+							if (addi == 1) {
+								box = 'O';												//バイト型で送るもの
+								buffer[0] = box;
+							}
+
+							if (addi > 300) {
+								ini = false;
+								addi = 0;
+								sleep = 0;
+							}
+						}
+
+
+
+					}
+				}
+
+				else {
+					sleep++;//カウントして
+				}
+
+				DrawFormatString(0, 0, GetColor(255, 255, 255), "x=%.1f y=%.1f z=%.1f", x, y, z);
+				DrawFormatString(0, 20, GetColor(255, 255, 255), "%.1f", fps.mFps);
+
+				
+
+				//DrawFormatString(50, 50, GetColor(255, 255, 255), "%3f.4,%3f.4,%3f.4", cx, cy, cz);
+
+				SetCameraPositionAndTarget_UpVecY(VGet(cx, cy, cz), VGet(tx, 0.0f, 0.0f));
+
+				ScreenFlip();
 			}
 		}
-
-		else {
-			sleep++;//カウントして
-		}
-
-			DrawFormatString(0, 0, GetColor(255, 255, 255), "x=%.1f y=%.1f z=%.1f", x, y, z);
-			DrawFormatString(0, 20, GetColor(255, 255, 255), "%.1f", fps.mFps);
-		}
+	}
 	DxLib_End();
 	return 0;
 }
